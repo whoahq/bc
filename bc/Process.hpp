3728c3d1670a5701cb55aa1fd0d9a65050b9160c
fallenoak <git@fallenoak.me>
2023-03-03 10:44:30 -0600

    feat(process): add basic process functions

---
+++ bc/Process.hpp
@@ -0,0 +1,15 @@
+#ifndef BC_PROCESS_HPP
+#define BC_PROCESS_HPP
+
+#include <cstdint>
+
+namespace Blizzard {
+namespace Process {
+
+// Functions
+void Sleep(uint32_t duration);
+
+} // namespace Process
+} // namespace Blizzard
+
+#endif
